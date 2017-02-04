7e6d0b69019b16f0ba0cc3bbe39c527442f454fc
todstoychev <todstoychev@gmail.com>
2017-02-04 16:12:55 +0200

    Translations fix

--- resources/views/event-location.blade.php
+++ resources/views/event-location.blade.php
@@ -1,27 +1,27 @@
 {{-- Longitude --}}
-<label for="longitude">@lang('calendar-events.longitude')</label>
+<label for="longitude">@lang('calendar-events::calendar-events.longitude')</label>
 <input type="text" 
        name="longitude"
        id="longitude"
-       placeholder="@lang('calendar-events.longitude')"
+       placeholder="@lang('calendar-events::calendar-events.longitude')"
        value="{{ (isset($calendarEvent) && !empty($calendarEvent->eventLocation->longitude)) ? $calendarEvent->eventLocation->longitude : null}}"
 />
 
 {{-- Latitude --}}
-<label for="latitude">@lang('calendar-events.latitude')</label>
+<label for="latitude">@lang('calendar-events::calendar-events.latitude')</label>
 <input type="text"
        name="latitude"
        id="latitude"
-       placeholder="@lang('calendar-events.latitude')"
+       placeholder="@lang('calendar-events::calendar-events.latitude')"
        value="{{ (isset($calendarEvent) && !empty($calendarEvent->eventLocation->latitude)) ? $calendarEvent->eventLocation->latitude : null}}"
 />
 
 {{-- Address --}}
-<label for="address">@lang('calendar-events.address')</label>
+<label for="address">@lang('calendar-events::calendar-events.address')</label>
 <input type="text"
        name="address"
        id="address"
-       placeholder="@lang('calendar-events.address')"
+       placeholder="@lang('calendar-events::calendar-events.address')"
        value="{{ (isset($calendarEvent) && !empty($calendarEvent->eventLocation->address)) ? $calendarEvent->eventLocation->address : null}}"
 />
 
