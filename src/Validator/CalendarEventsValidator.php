4e0fbee32c33fa92e66772c9f07b9f534f4e4fd1
Unknown <todstoychev@gmail.com>
2017-10-27 21:34:14 +0300

    logic changes and added unit tests
    - request changed to use get instead of input
    - relations call changed in model
    - validator method changed to return boolean
    - unit tests added

--- src/Validator/CalendarEventsValidator.php
+++ src/Validator/CalendarEventsValidator.php
@@ -24,7 +24,7 @@ class CalendarEventsValidator
      */
     public function validateTime($attribute, $value, array $parameters, Validator $validator)
     {
-        return preg_match('/([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?/', $value);
+        return (bool)preg_match('/([01]?[0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?/', $value);
     }
 
     /**
